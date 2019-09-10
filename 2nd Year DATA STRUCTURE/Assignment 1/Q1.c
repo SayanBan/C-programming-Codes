d4a9851f94b7e6ac720b02c9e3bac1ed46c9d255
Sayan bandyopadhyay <45221397+SayanBan@users.noreply.github.com>
2019-09-10 09:43:34 +0530

    Create Q1.c

---
+++ 2nd Year DATA STRUCTURE/Assignment 1/Q1.c
@@ -0,0 +1,16 @@
+# QUESTION
+
+**Implement the ADT Array using C programming language. Allowable range of
+data may be integers. Implement the functions like Display( ), Insert( ), Delete( ),
+Reverse( ), RemoveDuplicates( ), Replace( ) and Search( ). The first five functions
+are as discussed in the class. Replace( ) and Search( ) is defined as follows:**
+
+**Replace(A, n, x, y) – Element ‘x’ will be replaced by element ‘y’ in the array ‘A’
+of size ‘n’.**
+
+**Search(A, n, key) – the Array ‘A’ with ‘n’ elements be searched for the value
+‘key’. The function should return the first position of the occurrence of the ‘key’.
+Will return (-1), if the element is not present in the array.**
+
+# ANSWER
+
